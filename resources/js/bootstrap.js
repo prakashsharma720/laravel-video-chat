ab7475805f5b147b7e52ac48d7515f49d6d46608
prakashsharma720 <34154254+prakashsharma720@users.noreply.github.com>
2022-10-29 17:55:27 +0530

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -29,6 +29,7 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
  * allows your team to easily build robust real-time web applications.
  */
 
+
 import Echo from "laravel-echo";
 window.Pusher = require("pusher-js");
 window.Echo = new Echo({
